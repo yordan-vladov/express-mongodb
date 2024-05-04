89f9d7580fc2677358f1ac353b99bbc2856d9504
Yordan Vladov <jordan0vladov@gmail.com>
2024-05-04 13:16:36 +0300

    Added movie controller

---
+++ controllers/movie.controller.js
@@ -0,0 +1,73 @@
+const Movie = require('../models/movie.model');
+
+const getMovies = async (req, res) => {
+    try {
+        const movies = await Movie.find({});
+        res.status(200).json(movies);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
+const getMovie = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const movie = await Movie.findById(id);
+        res.status(200).json(movie);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+const createMovie = async (req, res) => {
+    try {
+        const movie = await Movie.create(req.body);
+        res.status(200).json(movie);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
+const updateMovie = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const movie = await Movie.findByIdAndUpdate(id, req.body);
+
+        if (!movie) {
+            return res.status(404).json({ message: "Movie not found" });
+        }
+
+        const updatedMovie = await Movie.findById(id);
+        res.status(200).json(updatedMovie);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+const deleteMovie = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const movie = await Movie.findByIdAndDelete(id);
+
+        if (!movie) {
+            return res.status(404).json({ message: "Movie not found" });
+        }
+
+        res.status(200).json({ message: "Movie deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
+module.exports = {
+    getMovies,
+    getMovie,
+    createMovie,
+    updateMovie,
+    deleteMovie,
+};
